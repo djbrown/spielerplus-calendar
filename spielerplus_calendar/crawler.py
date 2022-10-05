c3a5a83c5827b73bd9aa7b4ce043a1e95a182c7a
Daniel Brown <djbrown@users.noreply.github.com>
2022-10-05 21:30:57 +0200

    add old parameter to crawler

--- spielerplus_calendar/crawler.py
+++ spielerplus_calendar/crawler.py
@@ -3,15 +3,15 @@ import urllib
 import requests
 
 
-def fetch_event_list(server: str, identity: str, offset=0):
+def fetch_event_list(server: str, identity: str, offset=0, old=False) -> str:
     url = f"{server}/events/ajaxgetevents"
-    iterative = requests.post(
+    response = requests.post(
         url,
         cookies={"_identity": urllib.parse.quote(identity)},
-        data={"offset": offset},
+        data={"offset": offset, "old": str(old)},
         timeout=5,
-    ).json()
-    return iterative["count"]
+    )
+    return response.json()["html"]
 
 
 def fetch_event_calendar(server, identity: str, date: str = "2022-09-01") -> str:
