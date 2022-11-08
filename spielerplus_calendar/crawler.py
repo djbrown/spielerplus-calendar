b00dc9a795d8a5c63c821947de473650ad85e8b4
Daniel Brown <djbrown@users.noreply.github.com>
2022-11-08 18:56:09 +0100

    get all events from past and future

--- spielerplus_calendar/crawler.py
+++ spielerplus_calendar/crawler.py
@@ -3,15 +3,40 @@ import urllib
 import requests
 
 
-def fetch_event_list(server: str, identity: str, offset=0, old=False) -> str:
+def fetch_event_list(server: str, identity: str) -> str:
+    all_html = ""
+
+    offset = 0
+    count = 3
+    while count > 0:
+        (html, count) = _event_list(server, identity, offset=offset, old=True)
+        offset += count
+        all_html = html + all_html
+
+    offset = 0
+    count = 5
+    while count > 0:
+        (html, count) = _event_list(server, identity, offset=offset, old=False)
+        offset += count
+        all_html = all_html + html
+    return all_html
+
+
+def _event_list(server: str, identity: str, offset=0, old=False) -> tuple[str, int]:
     url = f"{server}/events/ajaxgetevents"
+
+    data = {"offset": offset}
+    if old:
+        data["old"] = "true"
+
     response = requests.post(
         url,
         cookies={"_identity": urllib.parse.quote(identity)},
-        data={"offset": offset, "old": str(old)},
+        data=data,
         timeout=5,
     )
-    return response.json()["html"]
+    json = response.json()
+    return (json["html"], json["count"])
 
 
 def fetch_event_calendar(server, identity: str, date: str = "2022-09-01") -> str:
