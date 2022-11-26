784d88940533218d1d7ecc347e54a9d5e3d3ccc4
Daniel Brown <djbrown@users.noreply.github.com>
2022-11-26 13:07:15 +0100

    refactor fetch event list

--- spielerplus_calendar/crawler.py
+++ spielerplus_calendar/crawler.py
@@ -4,47 +4,35 @@ import requests
 
 
 def fetch_event_list(server: str, identity: str) -> str:
+    html = _fetch_event_list(server, identity, old=True)
+    html += _fetch_event_list(server, identity, old=False)
+    return html
+
+
+def _fetch_event_list(server: str, identity: str, old: bool) -> str:
     all_html = ""
+    count = 3 if old else 5
+    offset = 0 if old else 5
 
-    offset = 0
-    count = 3
     while count > 0:
-        (html, count) = _event_list(server, identity, offset=offset, old=True)
-        offset += count
-        all_html = html + all_html
+        url = f"{server}/events/ajaxgetevents"
+        cookies = dict(_identity=urllib.parse.quote(identity))
+        data = {"offset": offset}
+        if old:
+            data["old"] = "true"
 
-    offset = 0
-    count = 5
-    while count > 0:
-        (html, count) = _event_list(server, identity, offset=offset, old=False)
+        response = requests.post(url, cookies=cookies, data=data, timeout=5)
+
+        json = response.json()
+        all_html += json["html"]
+        count = json["count"]
         offset += count
-        all_html = all_html + html
+
     return all_html
-
-
-def _event_list(server: str, identity: str, offset=0, old=False) -> tuple[str, int]:
-    url = f"{server}/events/ajaxgetevents"
-
-    data = {"offset": offset}
-    if old:
-        data["old"] = "true"
-
-    response = requests.post(
-        url,
-        cookies={"_identity": urllib.parse.quote(identity)},
-        data=data,
-        timeout=5,
-    )
-    json = response.json()
-    return (json["html"], json["count"])
 
 
 def fetch_event_calendar(server, identity: str, date: str = "2022-09-01") -> str:
     url = f"{server}/events/calendar?date={date}"
     cookies = dict(_identity=urllib.parse.quote(identity))
-    response = requests.post(
-        url,
-        cookies=cookies,
-        timeout=5,
-    )
+    response = requests.post(url, cookies=cookies, timeout=5)
     return response.text
