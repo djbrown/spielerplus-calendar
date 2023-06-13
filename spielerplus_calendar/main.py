1d53e92ad366e7f1356e6554d4abc9c5087fc9b6
Daniel Brown <djbrown@users.noreply.github.com>
2023-06-13 19:56:56 +0000

    fix order of str parameters for rendering
    see #3

--- spielerplus_calendar/main.py
+++ spielerplus_calendar/main.py
@@ -6,7 +6,7 @@ def team_calendar(server: str, identity: str, team_name) -> str:
     items = parsing.parse_event_calendar(html)
     filtered = appointment.filter_items(items)
     appointments = [appointment.from_calendar_item(item) for item in filtered]
-    calendar = rendering.to_icalendar(appointments, server, team_name)
+    calendar = rendering.to_icalendar(appointments, team_name, server)
     return calendar.to_ical().decode("utf-8")
 
 
@@ -33,7 +33,7 @@ def personal_calendar(server: str, identity: str, team_name) -> str:
         )
     ]
 
-    calendar = rendering.to_icalendar(appointments, server, team_name)
+    calendar = rendering.to_icalendar(appointments, team_name, server)
     return calendar.to_ical().decode("utf-8")
 
 
